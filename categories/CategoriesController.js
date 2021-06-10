6523ba091ea05c3aaff692bb9de988dd709b3ac7
WagnerReis <wagnerreis1997@gmail.com>
2021-06-10 16:28:52 -0300

    Deletando categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -28,4 +28,23 @@ router.get("/admin/categories", (req, res) => {
   })
 });
 
+router.post("/categories/delete", (req, res) => {
+  var { id } = req.body;
+  if(id != undefined) {
+    if(!isNaN(id)){
+      Category.destroy({
+        where: {
+          id : id
+        }
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    }else{//NÂO FOR UM NÚMERO
+      res.redirect("/admin/categories");
+    }
+  }else{ //NULL
+    res.redirect("/admin/categories");
+  }
+});
+
 module.exports = router;
