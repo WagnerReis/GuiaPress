5f9e663afb1bd55cf6827b96429deaee6f944938
WagnerReis <wagnerreis1997@gmail.com>
2021-06-11 10:22:48 -0300

    Ajustando tempo no sequelize

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,14 +4,29 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
-router.get("/articles",(req, res) => {
+router.get("/admin/articles",(req, res) => {
   res.send("Rota de artigos");
 });
 
 router.get("/admin/articles/new",(req, res) => {
   Category.findAll().then(categories => {
-    res.render("admin/articles/new", {categories});
+    res.render("admin/articles/new", {categories: categories});
   })
+});
+
+router.post("/articles/save", (req, res) => {
+  var { title } =  req.body;
+  var { body } =  req.body;
+  var { category } =  req.body;
+
+  Article.create({
+    title,
+    slug: slugify(title),
+    body,
+    categoryId: category
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
 
 module.exports = router;
